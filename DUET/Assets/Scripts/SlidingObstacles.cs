b683c62c955d2d32396aabd499f5671955c96771
Ankit Bourasi <ankitbourasi0@gmail.com>
2021-10-06 17:14:39 +0530

    sliding Obstacle with script

---
+++ DUET/Assets/Scripts/SlidingObstacles.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class SlidingObstacles : MonoBehaviour
+{
+    [SerializeField] float slidingDuration;
+    [SerializeField] float slidingDistance;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform
+            .DOLocalMoveX( slidingDistance ,slidingDuration)
+            .SetLoops(-1 , LoopType.Yoyo)
+            .SetEase(Ease.InOutBack);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
