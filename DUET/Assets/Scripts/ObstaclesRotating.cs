a199179e16dc5da4f5e4ab1b2852cd93f8efe369
Ankit Bourasi <ankitbourasi0@gmail.com>
2021-10-06 16:54:53 +0530

    obstacle Rotating with Script

---
+++ DUET/Assets/Scripts/ObstaclesRotating.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ObstaclesRotating : MonoBehaviour
+{
+
+    [SerializeField] float rotatingDuration;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform
+            .DORotate(new Vector3(0,0,1f), rotatingDuration)
+            .SetLoops(-1, LoopType.Incremental);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
