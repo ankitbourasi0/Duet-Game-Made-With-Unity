bd6fac47bd1e2df33aeb01c7d237ab01620a6fd0
Ankit Bourasi <ankitbourasi0@gmail.com>
2021-10-06 16:34:45 +0530

    explosion complete with script

--- DUET/Assets/Scripts/CollisionDetectionBalls.cs
+++ DUET/Assets/Scripts/CollisionDetectionBalls.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class CollisionDetectionBalls : MonoBehaviour
 {
+
+    ParticleSystem explosionFx;
+
+    void Start(){
+        explosionFx = transform.GetChild(0).GetComponent<ParticleSystem>();
+    }
     void OnCollisionEnter2D (Collision2D other){
         if(other.collider.CompareTag("Obstacles")){
             GameManger.Instance.isGameOver = true;
+            explosionFx.Play();
         PlayerMovement.Instance.Restart();
         }
     }
